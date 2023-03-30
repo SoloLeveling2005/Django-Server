af05c55bb028d512dd41740db4309f78c6e8b82a
SoloLeveling <SoloLevelingMansur2005@gmail.com>
2023-03-30 19:49:29 +0600

    30.03.2023

--- mysite/settings.py
+++ mysite/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-rc^*w^w&6g9_(uvx#6s*bnt!w)l0rdi%!l7mv#y%uc&x%wo5pk
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'https://django-server-production-eeea.up.railway.app']
+ALLOWED_HOSTS = ['127.0.0.1', 'https://django-server-production-eeea.up.railway.app', 'django-server-production-eeea.up.railway.app']
 CSRF_TRUSTED_ORIGINS = ['https://django-server-production-eeea.up.railway.app']
 
 # FORM SUBMISSION
